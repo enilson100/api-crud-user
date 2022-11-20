8d45abd1b2b4f45c797ab7dc89141fcab527e3b4
enilson silva <enilsonsilva999@gmail.com>
2022-11-20 11:20:48 -0300

    feat: create-list-user

---
+++ src/controller/user/listUsers.conroller.js
@@ -0,0 +1,9 @@
+import listUsersService from "../services/users/listUsers.service";
+
+const listUsersController = (req, res) => {
+  const listUsers = listUsersService();
+
+  return res.json(listUsers);
+};
+
+export default listUsersController;
